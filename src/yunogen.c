7ed792d7fb052b35ecdb5abdf87615eeb127e22e
Jussi Toivonen <jussi.e.toivonen@kapsi.fi>
2016-02-07 12:45:44 +0200

    Made the source refer to correct license file

--- src/yunogen.c
+++ src/yunogen.c
@@ -10,7 +10,7 @@
  * Copyright © 2015 bitofhope <bitofhope@kapsi.fi>
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
- * as published by Sam Hocevar. See the COPYING file for more details.
+ * as published by Sam Hocevar. See the LICCENSE file for more details.
  */
 
 #include <curses.h>
